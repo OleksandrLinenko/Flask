a075444f418927e47f9c1e8e010be9426ccdb9f0
Oleksandr Linenko <oleksandr.linenko@tul.cz>
2026-06-27 20:02:56 +0200

    Refactoring

--- src/app.py
+++ src/app.py
@@ -2,13 +2,13 @@ from flask import Flask
 from flask import request
 from markupsafe import escape
 from .routes.about import about_bp
-from src.routes.hellos import hellos_bp
+from src.routes.hello import hello_bp
 
 app = Flask(__name__)
 app.secret_key = "my_secret_key"
 
 app.register_blueprint(about_bp)
-app.register_blueprint(hellos_bp)
+app.register_blueprint(hello_bp)
 
 
 
--- src/routes/hello.py
+++ src/routes/hello.py
@@ -3,27 +3,27 @@ from flask import request
 from markupsafe import escape
 from flask import render_template
 from flask import flash, redirect, url_for
-from src.repositories.hellos_repository import HelloRepository
-from src.services.hellos_service import HelloService
+from src.repositories.hello_repository import HelloRepository
+from src.services.hello_service import HelloService
 
-hellos_bp = Blueprint("hellos", __name__)
-hellos_repo = HelloRepository()
-hellos_service = HelloService(hellos_repo)
+hello_bp = Blueprint("hello", __name__)
+hello_repo = HelloRepository()
+hello_service = HelloService(hello_repo)
 
-@hellos_bp.route("/")
+@hello_bp.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@hellos_bp.route("/hello")
+@hello_bp.route("/hello")
 def hello():
     name = request.args.get("name", "Flask")
     return f"Hello, {escape(name)}!"
 
-@hellos_bp.route('/hi/')
-@hellos_bp.route('/hi/<name>')
+@hello_bp.route('/hi/')
+@hello_bp.route('/hi/<name>')
 def hi(name=None):
-    message = hellos_service.greet(name)
-    users = hellos_service.get_users()
+    message = hello_service.greet(name)
+    users = hello_service.get_users()
 
     flash(message)
     return render_template("hello.html", users=users, person=name)
