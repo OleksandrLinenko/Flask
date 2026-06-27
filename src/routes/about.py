1bb2f49694c0ad4c822a5d82d59444181939187e
Oleksandr Linenko <oleksandr.linenko@tul.cz>
2026-06-27 14:18:26 +0200

    Add about template

--- src/routes/about.py
+++ src/routes/about.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
 from flask import request
+from flask import render_template
 about_bp = Blueprint("about", __name__)
 
 @about_bp.route("/about")
 def about():
-    name = request.args.get("name", "Flask")
-    return "<p>The project page of Flask training</p>"
+    return render_template("about_template.html")
