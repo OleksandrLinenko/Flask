28294ccc28652b72c643692a1dcf928d305a4fd6
Oleksandr Linenko <oleksandr.linenko@tul.cz>
2026-07-06 22:52:19 +0200

    Add authentication

--- src/models/user.py
+++ src/models/user.py
@@ -5,4 +5,5 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     
